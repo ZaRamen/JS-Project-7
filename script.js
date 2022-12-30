1b442a80841f3493c859a70d0b0b8140530736a8
ZaRamen <rlin7289@gmail.com>
2022-12-30 13:52:15 -0500

    finish

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ var timeObject = null;
 var timeLeft = 30;
 var score = 0   ;
 var pokemonQuizInfo = {};
+var hintCounter = 0;
+var currentHintName = "";
 
-
+function logKey(event)
+{
+    // console.log(event.which);
+    //enter key
+    if (event.which == 13)
+    {
+        guessPokemon();
+    }
+    //H key
+    else if (event.which == 104)
+    {
+        hint();
+    }
+    // space key
+    else if (event.which == 32)
+    {
+        nextPokemon(false);
+    }
+}
 async function fetchPokeAPI(url)
 {
     const response =  await fetch(url);
@@ -28,11 +48,11 @@ async function getPokemonInfo()
     }
     while(name.includes("-"))
 
-    console.log(pokemonData);
+    // console.log(pokemonData);
    
     pokemonQuizInfo['name'] = name.charAt(0).toUpperCase() + name.slice(1);
     pokemonQuizInfo['spriteURL'] = pokemonData['sprites']['front_default']; 
-    console.log(pokemonQuizInfo['name']);
+    // console.log(pokemonQuizInfo['name']);
 }
 
 
@@ -70,7 +90,7 @@ function decrement()
         }, 250);
         
     }
-    console.log(timeLeft);
+
     
     timeLeft--;
     
@@ -93,12 +113,69 @@ function guessPokemon()
     else
     {
         message.innerHTML = "Incorrect!";
-        console.log("wrong");
     }
     document.getElementById('guess').value = "";
 }
 function hint()
 {
+    let message = document.getElementById('message');
+    switch(hintCounter)
+    {
+        case 0:
+            message.innerHTML = "The Pokemon's name contains " + pokemonQuizInfo['name'].length + " characters";
+            hintCounter++;
+            break;
+        case 1:
+            message.innerHTML = "The Pokemon name starts with the letter " + pokemonQuizInfo['name'].charAt(0);
+            //reset the hint counter
+            hintCounter++;
+            break;
+        case 2:
+            //should not give multiple hint name, one per pokemon
+            if (currentHintName == "")
+            {
+                currentHintName = revealPartOfName(pokemonQuizInfo['name']);
+            }
+            message.innerHTML = "Hint is " + currentHintName.join(' ');
+            hintCounter++;
+            break;
+        case 3:
+            message.innerHTML = "The Pokemon's name contains " + pokemonQuizInfo['name'].length + " characters" + "<br>";
+            message.innerHTML += "The Pokemon name starts with the letter " + pokemonQuizInfo['name'].charAt(0) + "<br>";
+            message.innerHTML += "Hint is " + currentHintName.join(' ') + "<br>";
+            
+    }   
+}
+function revealPartOfName(name)
+{
+    let numbers = [];
+    let tmp;
+    let numberCounter = 0;
+    let hintName = Array.apply(null, Array(name.length));
+    hintName.fill("_");
+    for (let i = 0; i < Math.floor(name.length / 2); i++)
+    { 
+        do
+        {
+            let rand = Math.floor(Math.random() * name.length);
+            tmp = numbers.includes(rand);
+            if (!tmp)
+            {
+                //numbers array contains the index of the letters that we want to display
+                numbers.push(rand);
+                //don't sort the array as that can cause issues
+                //i.e let's say numbers[0] is [5], andn name[5] is 'l' so it  puts 'l' in hintName[5]
+                //then numbers appends a 1. It sorts and then numbers is [1, 5]
+                //but now numberCounter = 1
+                //we put 'l' in numbers[1] = name[numbers[1]] (this is name[5])               
+                hintName[rand] = name[numbers[numberCounter]];
+                numberCounter++;
+            }  
+        }
+        while(tmp)
+    }
+    return hintName;
+    
 
 }
 function nextPokemon(guessRight)
@@ -114,7 +191,8 @@ function nextPokemon(guessRight)
     //reset time and message
     timeLeft = 30;
     message.innerHTML = "";  
-    
+    hintCounter = 0;
+    currentHintName = "";
     //restart game
     startGame(); 
 }
