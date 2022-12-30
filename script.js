b49b28524bd2643c2bfd0bd218c638005d52a2b9
ZaRamen <rlin7289@gmail.com>
2022-12-30 03:01:20 -0500

    mostly done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var jsonFile;
-var count;
+var timeObject = null;
+var timeLeft = 30;
 var score = 0   ;
 var pokemonQuizInfo = {};
 
@@ -11,20 +12,16 @@ async function fetchPokeAPI(url)
     // console.log(data);
     return data;
 }
-function timer()
-{
-
-}
-
-
 async function getPokemonInfo()
 {
     let name;
     let pokemonData;
+    //some pokemon have - in their name, don't include them in the pool
     do
     {
+        //choose random pokemon
         let rand = Math.floor(Math.random() * jsonFile["results"].length);
-        console.log(jsonFile["results"][rand]);
+        // console.log(jsonFile["results"][rand]);
         name = jsonFile["results"][rand]["name"];
         let url = jsonFile["results"][rand]["url"];
         pokemonData = await fetchPokeAPI(url);
@@ -32,48 +29,87 @@ async function getPokemonInfo()
     while(name.includes("-"))
 
     console.log(pokemonData)
-    pokemonQuizInfo['name'] = name;
+    pokemonQuizInfo['name'] = name.charAt(0).toUpperCase() + name.slice(1);
     pokemonQuizInfo['spriteURL'] = pokemonData['sprites']['front_default'];
 }
 
 async function startGame()
 {
+    
     await getPokemonInfo();
+    //start timer after image shows up
+    startTimer();
     display();
 }
-
 function display()
 {
     document.getElementById('pokemon-image').src = pokemonQuizInfo['spriteURL'];
 }
+function startTimer()
+{
+    document.getElementById("timer").innerHTML = "Time Remaining: " + timeLeft + " seconds";
+    timeObject = setInterval(decrement, 1000);
+}
+
+function decrement()
+{
+    document.getElementById("timer").innerHTML = "Time Remaining: " + timeLeft + " seconds";
+    timeLeft--;
+    if (timeLeft == 0)
+    {
+        alert("You ran out of time");
+        nextPokemon();
+    }
+}
+
+//button choices 
 function guessPokemon()
 {
+    let message = document.getElementById('message');
     if (document.getElementById('guess').value.toLowerCase() == pokemonQuizInfo['name'].toLowerCase())
     {
-        console.log("right");
+        message.innerHTML = "Correct!";
         score++;
         document.getElementById('score').innerHTML = "Score: " + score;
-        nextPokemon();
+        setTimeout(function()
+        {
+            nextPokemon(true);
+        }, 200);   
     }
     else
     {
+        message.innerHTML = "Incorrect!";
         console.log("wrong");
     }
     document.getElementById('guess').value = "";
 }
-function nextPokemon()
+function nextPokemon(guessRight)
 {
-    startGame();
+    //clear timer
+    clearInterval(timeObject);
+
+    //show answer if they skipped the pokemon
+    if (!guessRight)
+    {
+        alert("Correct Pokemon was " + pokemonQuizInfo['name']);
+    }
+    //reset time and message
+    timeLeft = 30;
+    message.innerHTML = "";  
+    
+    //restart game
+    startGame(); 
 }
 
 document.addEventListener('DOMContentLoaded',  async function()
 {
     //get all the pokemon
     let url = "https://pokeapi.co/api/v2/pokemon/?limit=1154&";
+    //contains all pokemon data
     jsonFile = await fetchPokeAPI(url)
-    console.log(jsonFile);
+    // console.log(jsonFile);
 
-    //choose a random pokemon
     startGame();
+  
 });
 
